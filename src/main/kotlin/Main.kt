a188638bcbd9d2a6a466c58d490f09382ca6d644
acepele <acepele@students.mchenry.edu>
2022-08-18 23:46:01 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("I am Aljeda.")
+    println("I like programming. I have been programming since 2015.")
+    println("This is my first time using Kotlin and it look really fun and interesting!")
+
+
+}
